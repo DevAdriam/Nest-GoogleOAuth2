73cc6657e4b70d3c6c90c11a16060d5cfdf8dd4b
naingaungzaw <naingaung9863@gmail.com>
2023-10-14 16:50:02 +0630

    added : GoogleAuthGuard

--- src/auth/utils/GoogleStrategy.ts
+++ src/auth/utils/GoogleStrategy.ts
@@ -1,6 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-google-oauth20';
-
+import { Strategy, Profile } from 'passport-google-oauth20';
+@Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
   constructor() {
     //set up OAuth client props
@@ -9,6 +10,14 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
       clientSECRET: process.env.CLIENT_SECRET,
       callbackURL: process.env.CALLBACK_URL,
       scope: [process.env.SCOPE_1, process.env.SCOPE_2],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    console.log({
+      accessToken,
+      refreshToken,
+      profile,
     });
   }
 }
